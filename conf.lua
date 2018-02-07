95b19026f32fbcbc0f226b43c328ccc824f743f4
F483 <f483@protonmail.com>
2018-02-07 16:46:21 +0100

    Release candidate 1

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
-    t.identity = "tanzboy_dev"             -- The name of the save directory (string)
+    t.identity = "tanzlove"            -- The name of the save directory (string)
     t.version = "0.10.1"               -- The LÖVE version this game was made for (string)
     t.console = true                   -- Attach a console (boolean, Windows only)
 
-    t.window.title = "TANZBOY"         -- The window title (string)
+    t.window.title = "TANZ.LOVE"       -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 640               -- The window width (number)
     t.window.height = 400              -- The window height (number)
